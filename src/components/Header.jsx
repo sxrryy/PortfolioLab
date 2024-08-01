ec8c2d3977b19cc56f65941941e97b7aaf184854
sxrryy <sxrryy@gmail.com>
2024-08-01 10:45:58 +0200

    header layout fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Header() {
+
+  const linkClasses = "text-black text-lg mx-1 pl-3 pr-3 pt-1 pb-1 border-2 border-transparent hover:border-black transition-colors duration-300";
+
   const scrollToSection = (elementId) => {
     const element = document.getElementById(elementId);
     if (element) {
@@ -13,27 +16,27 @@ function Header() {
     <header className="bg-white p-4">
       <nav className="container mx-auto flex flex-col items-end">
         <div className="flex justify-end w-full mb-2">
-          <Link to="/login" className="text-black text-lg mx-2">
+          <Link to="/login" className={linkClasses}>
             Zaloguj
           </Link>
-          <Link to="/register" className="text-black text-lg mx-2">
+          <Link to="/register" className={linkClasses}>
             Załóż konto
           </Link>
         </div>
         <div className="flex justify-end w-full">
-          <Link to="/" className="text-black text-lg mx-2">
+          <Link to="/" className={linkClasses}>
             Start
           </Link>
-          <a href="#steps" onClick={() => scrollToSection('steps')} className="text-black text-lg mx-2">
+          <a href="#steps" onClick={() => scrollToSection('steps')} className={linkClasses}>
             O co chodzi?
           </a>
-          <a href="#aboutus" onClick={() => scrollToSection('aboutus')} className="text-black text-lg mx-2">
+          <a href="#aboutus" onClick={() => scrollToSection('aboutus')} className={linkClasses}>
             O nas
           </a>
-          <a href="#organizations" onClick={() => scrollToSection('organizations')} className="text-black text-lg mx-2">
+          <a href="#organizations" onClick={() => scrollToSection('organizations')} className={linkClasses}>
             Fundacja i organizacje
           </a>
-          <a href="#contact" onClick={() => scrollToSection('contact')} className="text-black text-lg mx-2">
+          <a href="#contact" onClick={() => scrollToSection('contact')} className={linkClasses}>
             Kontakt
           </a>
         </div>
